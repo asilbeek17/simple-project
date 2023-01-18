c3ad5019737083b8ae78e2303a47958af0bec962
asilbeek17 <120719876+asilbeek17@users.noreply.github.com>
2023-01-18 21:56:56 +0500

    second commit

--- app/form.py
+++ app/form.py
@@ -1,21 +1,10 @@
-# from django.contrib.sites.shortcuts import get_current_site
-# from django.core.exceptions import ValidationError
-# from django.core.mail import send_mail
-# from django.forms import *
-# from django import forms
-# from django.template.loader import render_to_string
-# from django.utils.encoding import force_bytes
-# from django.utils.http import urlsafe_base64_encode
-# from root.settings import EMAIL_HOST_USER
+from django.forms import ModelForm
+from app.models import Post
 
 
-# class ForgotPasswordForm(forms.Form):
-#     email = forms.EmailField()
-#
-#     def send_email(self):
-#         subect = 'xabarni titlesi'
-#         message = 'message ham keldi'
-#         from_email = EMAIL_HOST_USER
-#         recipient_list = ['asilbek1721@gmail.com']
-#         result = send_mail(subect, message, from_email, recipient_list)
-#         print(result)
+class ProductModelForm(ModelForm):
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+        exclude = ()
--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, FormView
 # from app.form import ForgotPasswordForm
+from .form import ProductModelForm
 from .models import Post
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -14,15 +15,15 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class BlogCreatview(CreateView):
-    model = Post
-    template_name = 'post_new.html'
-    fields = ['image', 'title', 'body', 'author']
-    success_url = reverse_lazy('home')
+# class BlogCreatview(CreateView):
+#     model = Post
+#     template_name = 'post_new.html'
+#     fields = ['image', 'title', 'body', 'author']
+#     success_url = reverse_lazy('home')
 
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
+    # def form_valid(self, form):
+    #     form.save()
+    #     return super().form_valid(form)
 
 
 class BlogUpdateView(UpdateView):
@@ -41,12 +42,30 @@ def others(reqest):
 def CalculatorView(request):
     return render(request, 'calculator/calculator_1.html')
 
-# def registered(request):
-#     return render(request, 'registered.html')
-
 class RegisterePeople(ListView):
     model = User
     template_name = 'registered.html'
+
+# ---------------------------------------------------------------------
+
+def create_product(request):
+    print(1121)
+    if request.method == "POST":
+
+        form = ProductModelForm(request.POST)
+        if form.is_valid():
+
+            form.save()
+            success_url = reverse_lazy('home')
+
+
+    form = ProductModelForm()
+    context = {
+        'form': form
+    }
+
+    return render(request, 'CRUD/create_product.html', context)
+
 
 
 
@@ -59,3 +78,4 @@ class RegisterePeople(ListView):
 #     def from_valid(self, form):
 #         form.send_email()
 #         return super().form_valid(form)
+
